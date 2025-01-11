8de91ede4b30292d520fc35da37079cfcb751836
Valentin Melusson <valentin.melusson@datadoghq.com>
2025-01-11 20:05:24 +0100

    [BUDGETORY] Initial commit

---
+++ components/NewTodo.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import supabase from '../utils/supabase'
+
+const NewTodo = ({ reload }) => {
+  const [title, setTitle] = useState('')
+
+  const addTodo = async (e) => {
+    e.preventDefault()
+    await supabase.from('todos').insert({ title })
+    reload()
+    setTitle('')
+  }
+
+  return (
+    <form onSubmit={addTodo}>
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+    </form>
+  )
+}
+
+NewTodo.displayName = 'NewTodo'
+export default NewTodo
